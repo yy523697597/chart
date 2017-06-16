b6c39cc22ea83263f14e3bc39e475701669b29f3
yy523697597 <523697597@qq.com>
2017-06-16 14:52:50 +0800

    V1.8 完成水平柱状图的开发

--- js/h5ComponentBar.js
+++ js/h5ComponentBar.js
@@ -5,15 +5,23 @@ var H5ComponentBar = function (name, cfg) {
 		console.log(item)
 		var line = $('<div class = "line"></div>');
 		var name = $('<div class = "name">' + item[0] + '</div>');
-		var rate = $('<div class ="rate">' + item[1] + '</div>');
+		var rate = $('<div class ="rate"></div>');
 		var bg = $('<div class="bg"></div>');
 		var per = $('<div class ="per">' + (item[1] * 100) + '%</div>');
-		bg.css('background-color', item[2]);
+		if (item[2]) {
+			bg.css('background-color', item[2]);
+		}
 		// 根据不同的比例去设定rate的宽度
 		rate.css('width', item[1] * 100 + '%');
 		rate.append(bg);
 		line.append(name).append(rate).append(per);
 		component.append(line);
+	});
+	component.on('onLoad', function () {
+		component.addClass('h5-component-bar-onload').removeClass('h5-component-bar-onleave');
+	});
+	component.on('onLeave', function () {
+		component.addClass('h5-component-bar-onleave').removeClass('h5-component-bar-onload');
 	});
 	return component;
 };
