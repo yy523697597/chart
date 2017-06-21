405d6deb8e360ba1516c1351b9155572b010bbb5
yy523697597 <523697597@qq.com>
2017-06-21 15:18:11 +0800

    V3.3 完成首页开发

--- js/h5ComponentBase.js
+++ js/h5ComponentBase.js
@@ -1,6 +1,6 @@
 //基本图文组件对象
-var H5ComponentBase = function(name, cfg) {
-	var cfg = cfg || {};
+var H5ComponentBase = function (name, cfg) {
+	cfg = cfg || {};
 	var id = ('h5_c_' + Math.random()).replace('.', '_');
 	var cls = 'h5-component-' + cfg.type + ' h5-component-name-' + name;
 	var $component = $('<div class="h5-component ' + cls + '" id="' + id + '">');
@@ -9,21 +9,29 @@ var H5ComponentBase = function(name, cfg) {
 	cfg.height && $component.height(cfg.height / 2);
 	cfg.css && $component.css(cfg.css);
 	cfg.bg && $component.css('backgroundImage', 'url(' + cfg.bg + ')');
-	if(cfg.center) {
+	if (cfg.center) {
 		$component.css({
 			marginLeft: (cfg.width / 4 * -1) + 'px',
 			left: '50%'
 		});
 	}
-	$component.on('onLeave', function() {
-		$component.addClass('h5-component-onLeave').removeClass('h5-component-onLoad');
-		cfg.animateOut && $component.animate(cfg.animateOut);
-		return false;
+	$component.on('onLeave', function () {
+
+		setTimeout(function () {
+			$component.addClass('h5-component-onLeave').removeClass('h5-component-onLoad');
+			cfg.animateOut && $component.animate(cfg.animateOut);
+			return false;
+		}, cfg.delay || 0);
+
 	});
-	$component.on('onLoad', function() {
-		$component.addClass('h5-component-onLoad').removeClass('h5-component-onLeave');
-		cfg.animateIn && $component.animate(cfg.animateIn);
-		return false;
+	$component.on('onLoad', function () {
+		setTimeout(function () {
+			$component.addClass('h5-component-onLoad').removeClass('h5-component-onLeave');
+			cfg.animateIn && $component.animate(cfg.animateIn);
+			return false;
+		}, cfg.delay || 0);
+
 	});
+
 	return $component;
 };
